67114276142f3ce295a9296cf3664b2bb9036c15
Vladimir-Moskalenko1 <moskalenkovladimir1998@gmail.com>
2022-04-20 18:17:32 +0200

    Base changes

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import os
+import random
 from dotenv import load_dotenv
+from discord.ext import commands, tasks
 from Dandy import Dandy_bot
 
 
-def main():
-    load_dotenv()
-    token = os.getenv('Token')
-    bot = Dandy_bot(token)
-    bot.awaken()
+load_dotenv()
+token = os.getenv('Token')
+bot = commands.Bot(command_prefix='!')
+Dandy = Dandy_bot()
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+@bot.command(name='roll_dice', help='Simulates rolling dice.')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
 
 
 if __name__ == "__main__":
-    main()
+    bot.run(token)
