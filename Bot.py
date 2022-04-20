fc67ce8acb7ef35190e8cf784598d59db50af9a0
Vladimir-Moskalenko1 <moskalenkovladimir1998@gmail.com>
2022-04-20 16:26:57 +0200

    README corrected

---
+++ Bot.py
@@ -0,0 +1,7 @@
+import discord
+import os
+
+from discord.ext import commands
+from config import settings
+
+client = commands.Bot(command_prefix="!")
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 
-def print_hi(name):
-    print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pass
